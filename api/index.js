2e0a4cc85ff83b2ad15ea7dff88090f999e77c43
andriarif <55911915+andriarif@users.noreply.github.com>
2026-04-30 13:44:23 +0700

    Refactor API request and response handling
    Updated the request body to use req.body and modified the response handling.

--- api/index.js
+++ api/index.js
@@ -7,25 +7,14 @@ export default async function handler(req, res) {
         "Authorization": "Bearer " + process.env.TOKEN,
         "Accept": "application/json, text/plain, */*",
         "Ar-Origin": "https://lopmiva.com",
-        "Referer": "https://lopmiva.com/#/home/AllLotteryGames/WinGo?id=1"
+        "Referer": "https://lopmiva.com/"
       },
-      body: JSON.stringify({
-        pageSize: 10,
-        pageNo: 1,
-        typeId: 30,
-        language: 1,
-        random: Math.random().toString(16).slice(2),
-        signature: "DUMMY",
-        timestamp: Math.floor(Date.now() / 1000)
-      })
+      body: JSON.stringify(req.body)
     });
 
-    const text = await response.text();
+    const data = await response.text();
 
-    res.status(200).json({
-      status: "RESPON RAW",
-      data: text
-    });
+    res.status(200).send(data);
 
   } catch (err) {
     res.status(500).json({
